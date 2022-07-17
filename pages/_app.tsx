59689a80510df7f22d871634e0c60b950e4d675f
automateeverythingM <markospavicbg@gmail.com>
2022-07-17 11:44:16 +0200

    init react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import type { AppProps } from "next/app";
+
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import { theme } from "../src/theme/theme";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider theme={theme} resetCSS>
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider theme={theme} resetCSS>
+				<Component {...pageProps} />
+				<ReactQueryDevtools />
+			</ChakraProvider>
+		</QueryClientProvider>
 	);
 }
 
